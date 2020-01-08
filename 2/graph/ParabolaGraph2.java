09eeba60ea245cba82fe880c96fca9bfa5bb8582
Ryota Eisaki <ryotaeisaki0530@icloud.com>
2020-01-08 14:19:28 +0900

    graph2

---
+++ 2/graph/ParabolaGraph2.java
@@ -0,0 +1,8 @@
+class ParabolaGraph2 extends ParabolaGraph {
+    void drawLine(int x, int y) {
+         System.out.println(x + " " + y);
+    }
+    public static void main(String[] args) {
+        (new ParabolaGraph2()).drawAll();
+    }
+}
