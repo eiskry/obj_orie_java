a0d94e7c8637e0abedfa42e02a54fd30a7442115
RyotaEisaki <39875637+RyotaEisaki@users.noreply.github.com>
2020-01-08 12:24:31 +0900

    Add files via upload

---
+++ 1/task2/RandomWalk.java
@@ -0,0 +1,19 @@
+public class RandomWalk {
+    public static void main(String[] args) {
+        SimpleRobot robot = new SimpleRobot();
+        for (int i = 0; i < 10; i++) {
+            robot.moveTo(0, 0);
+            robot.setHeading(0);
+            for (int j = 0; j < 10; j++) {
+                if (Math.random() >= 0.5) {
+                    robot.turnLeft(20);
+                } else {
+                    robot.turnRight(20);
+                }
+                robot.moveForward(-100);
+                robot.turnLeft(20);             
+
+            }
+        }
+    }
+}
