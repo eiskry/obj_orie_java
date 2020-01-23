9be6974e75e012e379f237cae1e3e24af2f41b50
Ryota Eisaki <ryotaeisaki0530@icloud.com>
2020-01-23 12:43:53 +0900

    edit

--- 2/robot/DotView.java
+++ 2/robot/DotView.java
@@ -4,52 +4,17 @@ interface RobotView {
                      double newX, double newY);
 }
 
-public class DotView implements RobotView {
-    public void turn(double oldHeading, double newHeading){
-    }
-    
-    public void move(double oldX, double oldY,double newX, double newY){
-        //何等分するか
-        int t=5;
-        
-        //y軸と平行 newX==oldX
-        // if ( (Math.abs((newX-oldX))) < (1/(10*10*10)) ){
-        if (newX==oldX){
-            newX=oldX;
-            for(int i=0;i<t;i++){
-                double Y =(newY-oldY)/t*i;
-                System.out.println(oldX+" "+(oldY+Y));
-            }
-            System.out.println(oldX+" "+newY);
-        }
-        // x軸と平行 newY==oldY
-        // else if( (Math.abs((newY-oldY))) < (1/(10*10*10)) ){
-        else if(newY==oldY){
-            newY=oldY;
-            for(int i=0;i<t;i++){
-                double X =(newX-oldX)/t*i;
-                System.out.println((oldX+X)+" "+newY);
-            }
-            System.out.println(newX+" "+oldY);
-        }
-        //直線
-        else {
-            //移動する前、移動した後を結ぶ直線
-            //y=Ax+B
-            double A=(newY-oldX)/(newX-oldX);
-            double B=oldY-A*oldX;
-            for(int i=0;i<t;i++){
-                double X =(newX-oldX)/t*i;
-                double Y =A*(oldX+X)+B;
-                System.out.println((oldX+X)+" "+Y);
-                // System.out.print(String.format("%.1f", oldX+X));
-                // System.out.print(" ");
-                // System.out.println(String.format("%.1f", Y));
-            }
-            System.out.println(newX+" "+newY);
-            // System.out.print(String.format("%.1f", newX));
-            // System.out.print(" ");
-            // System.out.println(String.format("%.1f", newY));
-        }
-    }
+class DotView implements RobotView {
+	public void turn(double oldHeading, double newHeading) {
+
+	}
+
+	public void move(double oldX, double oldY,
+			double newX, double newY) {
+		for (int i = 0; i < 6; i++) {
+			double p = (newX - oldX) / 5;
+			double q = (newY - oldY) / 5;
+			System.out.println((oldX + p * i) + " " + (oldY + q * i));
+		}
+	}
 }
--- 2/robot/SimpleView.java
+++ 2/robot/SimpleView.java
@@ -5,12 +5,14 @@ interface RobotView {
 }
 
 public class SimpleView implements RobotView {
-    public void turn(double oldHeading, double newHeading){
-    }
-    
-    public void move(double oldX, double oldY,
-                        double newX, double newY){
-        System.out.println(oldX+" "+ oldY);
-        System.out.println(newX+" "+ newY);
-    }
+
+	public void turn(double oldHeading, double newHeading) {
+
+	}
+
+	public void move(double oldX, double oldY,
+			double newX, double newY) {
+		System.out.println(oldX + " " + oldY);
+		System.out.println(newX + " " + newY);
+	}
 }
