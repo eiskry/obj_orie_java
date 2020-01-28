97e65b142ab71c6b3eb5cbe65b17b0a23b6d1d39
MacBookPro <ryotaeisaki0530@icloud.com>
2020-01-28 15:57:17 +0900

    edit

--- 3/map/FibOpt.java
+++ 3/map/FibOpt.java
@@ -1,14 +1,13 @@
 //Fibonacci Series using Optimized Method 
 class FibOpt 
 { 
-	/* function that returns nth Fibonacci number */
 	static int fib(int n) 
 	{ 
-	int F[][] = new int[][]{{1,1},{1,0}}; 
+	int F[][] = new int[][]{{0,1},{1,1}}; 
 	if (n == 0) return 0; 
 	power(F, n-1); 
 	
-	return F[0][0]%10000; 
+	return (F[0][0]+F[0][1])%1000; 
 	} 
 	
 	static void multiply(int F[][], int M[][]) 
@@ -24,19 +23,18 @@ class FibOpt
 	F[1][1] = w; 
 	} 
 	
-	/* Optimized version of power() in method 4 */
-	static void power(int F[][], int n) 
-	{ 
-	if( n == 0 || n == 1) 
-	return; 
-	int M[][] = new int[][]{{1,1},{1,0}}; 
-	
-	power(F, n/2); 
-	multiply(F, F); 
-	
-	if (n%2 != 0) 
-	multiply(F, M); 
-	} 
+    static void power(int F[][], int n) 
+    { 
+    if( n == 0 || n == 1) 
+      return; 
+    int M[][] = new int[][]{{0,1},{1,1}}; 
+       
+    power(F, n/2); 
+    multiply(F, F); 
+       
+    if (n%2 != 0) 
+       multiply(F, M); 
+    } 
 	
 	/* Driver program to test above function */
 	public static void main (String args[]) 
