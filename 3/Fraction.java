4334dc343c360c5c7cdef0ce9c8d43ee6a38cb4f
Ryota Eisaki <ryotaeisaki0530@icloud.com>
2020-01-29 10:27:35 +0900

    edit

--- 3/Fraction.java
+++ 3/Fraction.java
@@ -3,10 +3,30 @@ import java.io.*;
 
 public class Fraction{
 
-    static long calcu(int m, int n) {
-        if((m%n)==0){
-            return m/n;
+    //culc x^y;
+    static int power(int x, int y){
+        int answer =1;
+        for (int i=0;i<y;i++){
+            t *=x;
         }
+        return answer;
+    }
+    static double calcu(int x, int y) {
+        double m = (double)x;
+        double n = (double)y;
+        while(int i=0;i<15;i++){
+            int tmp_q =m/n;
+            int tmp_r =m-n*q;
+            double q=tmp_q/power(10, i);
+            
+        }
+
+        // if((m%n)==0){
+            double answer = (double)m/(double)n;
+        //     return answer;
+        // } else if {
+
+        // }
     }
     public static void main(String[] args){
         if (args.length == 2) {
@@ -15,8 +35,8 @@ public class Fraction{
             int i1 = inte1.intValue();
             int i2 = inte2.intValue();
             System.out.println("Calculate "+ i1+" / "+i2);
-            long answer=calcu(i1,i2);
-            System.out.println(answer);
+            double final_answer=calcu(i1,i2);
+            System.out.println(final_answer);
         } else System.out.println("Please input two number!");
     }   
 }
---
+++ 3/test.java
@@ -0,0 +1,11 @@
+class test{
+  public static void main(String args[]){
+    int i = 7;
+
+    double f1 = (double)i / 3;
+    double f2 = (double)(i / 3);
+
+    System.out.println("f1 = " + f1);
+    System.out.println("f2 = " + f2);
+  }
+}
