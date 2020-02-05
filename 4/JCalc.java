5aa1eb2643299f60d0be70d67abf2c5cdb78a1fc
Ryota Eisaki <ryotaeisaki0530@icloud.com>
2020-02-05 10:04:38 +0900

    edit

--- 4/JCalc.java
+++ 4/JCalc.java
@@ -1,15 +1,18 @@
 import java.util.*;
 
 public class JCalc extends OOCalc {
-    // class NoOp extends Op {
-    
-    // }
+    class NoOp extends Op {
+        public String opName() {return "noop";}
+        public void exec(Deque<Integer> stack) {}
+    }
+
     String[][] alias = {{"足す", "+"}, {"引く", "-"}, {"かける", "*"}, {"割る", "/"},
                         {"から", "noop"}, {"と", "noop"}, {"を", "noop"}, {"で", "noop"}};
     
     //ops:単語辞書　Map<String, Op> ops; 
     JCalc() {
         for (String[] pair : alias) {
+            ops.put("noop", new NoOp());
             ops.put(pair [0] , ops.get (pair [1]));
         }
     }
--- 4/RatioCalc.java
+++ 4/RatioCalc.java
@@ -6,43 +6,26 @@ public class RatioCalc extends OOGCalc<RatioCalc.Rat> {
     // (名前がぶつからないこと以外は，RatioCalcの外にRatクラスを定義するのとほぼ同じ．)
     static class Rat implements Numeric<Rat> {
         Ratio ratio;
-        Rat (int i) {
-            ratio = new Ratio(i, 1);
-        }
-        Rat (Ratio r) {
-            ratio = r;
-        }
-        public Rat add(Rat r) {
-            return new Rat (ratio.add (r.ratio));
-        }
-        public Rat subtract (Rat r) {
-            return new Rat (ratio.subtract (r.ratio));
-        }
-        public Rat multiply (Rat r) {
-            return new Rat (ratio.multiply (r.ratio));
-        }
-        public Rat divide (Rat r) {
-            return new Rat (ratio.divide (r.ratio));
-        }
-
-        public String toString () {
-            return ratio.toString ();
-        }
+        Rat (int i) { ratio = new Ratio(i, 1); }
+        Rat (Ratio r) { ratio = r; }
+        public Rat add(Rat r) { return new Rat (ratio.add (r.ratio)); }
+        public Rat subtract (Rat r) { return new Rat (ratio.subtract (r.ratio)); }
+        public Rat multiply (Rat r) { return new Rat (ratio.multiply (r.ratio)); }
+        public Rat divide (Rat r) { return new Rat (ratio.divide (r.ratio)); }
+        public String toString () { return ratio.toString (); }
     }
     protected Rat fromInt(int v) {
         return new Rat(v);
     }
     class NoOp extends Op {
-        public String opName(){
-            return "noop";
-        }    
+        public String opName(){ return "noop"; }    
         public void exec (Deque<Rat> stack){} 
     }
     String[][] alias = {{"足す", "+"}, {"引く", "-"}, {"かける", "*"}, {"割る", "/"},
                         {"から", "noop"}, {"と", "noop"}, {"を", "noop"}, {"で", "noop"}};
     RatioCalc() {
-        ops.put ("noop", new NoOp ());
         for (String [] pair : alias) {
+            ops.put ("noop", new NoOp ());
             ops.put (pair [0], ops.get (pair [1]));
         }
     }
